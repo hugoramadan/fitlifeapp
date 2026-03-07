1fa69bfd6f10ccebd25878d173c15f02d4ffc554
AndreHilario <andre18hilario@gmail.com>
2026-03-07 14:45:43 -0300

    feat: add new page to control the tasks on chart

--- evolucao.php
+++ evolucao.php
@@ -51,7 +51,7 @@ for ($i = 6; $i >= 0; $i--) {
     <title>FitLife - Evolução</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="css/style.css">
     <style>
         body {
             background-color: #f8f9fa;
